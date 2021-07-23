a9528c93424bd9f2fd246dc9f318dd7429f91756
Mark <mark@example.dev>
2021-07-23 15:54:51 -0700

    calc complete

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,20 @@
 # Put your app in here.
+import operations
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+op = {
+    "add" : operations.add,
+    "sub" : operations.sub,
+    "mult" : operations.mult,
+    "div" : operations.div
+}
+
+@app.route('/<operation>')
+def do_math(operation):
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    fn = op[operation]
+    return f"{fn(a,b)}"
